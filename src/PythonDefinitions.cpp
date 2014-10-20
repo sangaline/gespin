9afcaf653fffc335e35091227b6200882e1d3217
Evan Sangaline <esangaline@gmail.com>
2014-10-20 16:04:32 -0400

    Modified the python interface to Nucleon to use properties rather than functions (for x, y, z, position).

--- src/PythonDefinitions.cpp
+++ src/PythonDefinitions.cpp
@@ -2,6 +2,7 @@
 #include <boost/python.hpp>
 #include <boost/python/module.hpp>
 #include <boost/python/tuple.hpp>
+#include <boost/python/extract.hpp>
 
 //gespin include files
 #include "Nucleon.h"
@@ -10,8 +11,13 @@ using namespace boost::python;
 
 /***************** Nucleon helpers *********************/
 namespace NucleonHelpers {
-    tuple Position(Nucleon& nucleon) {
+    tuple GetPosition(Nucleon& nucleon) {
         return make_tuple(nucleon.X(), nucleon.Y(), nucleon.Z());
+    }
+    void SetPosition(Nucleon& nucleon, tuple position) {
+        nucleon.SetPosition(extract<float>(position[0]),
+                            extract<float>(position[1]),
+                            extract<float>(position[2]));
     }
 }
 /********************************************************/
@@ -21,10 +27,10 @@ BOOST_PYTHON_MODULE(core)
 /***************** Nucleon class ***********************/
     class_<Nucleon>("Nucleon", init<double, double, double>())
         .def(init<>())
-        .def("X", &Nucleon::X)
-        .def("Y", &Nucleon::Y)
-        .def("Z", &Nucleon::Z)
-        .def("position", &NucleonHelpers::Position)
+        .add_property("x", &Nucleon::X, &Nucleon::SetX)
+        .add_property("y", &Nucleon::Y, &Nucleon::SetY)
+        .add_property("z", &Nucleon::Z, &Nucleon::SetZ)
+        .add_property("position", &NucleonHelpers::GetPosition, &NucleonHelpers::SetPosition)
     ;
 /********************************************************/
 }
