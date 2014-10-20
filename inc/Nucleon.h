1dd364421b038dc8372cd77341f956c460250bf3
Evan Sangaline <esangaline@gmail.com>
2014-10-20 18:17:46 -0400

    Added r, theta, phi getters and setter for nucleons.

--- inc/Nucleon.h
+++ inc/Nucleon.h
@@ -9,8 +9,15 @@ class Nucleon {
     double X() { return x; }
     double Y() { return y; }
     double Z() { return z; }
+    double R();
+    double Theta();
+    double Phi();
+
     void SetX(double newx) { x = newx; }
     void SetY(double newy) { y = newy; }
     void SetZ(double newz) { z = newz; }
+    void SetR(double new_r);
+    void SetTheta(double new_theta);
+    void SetPhi(double new_phi);
     void SetPosition(double newx, double newy, double newz) { x = newx; y = newy; z = newz; }
 };
