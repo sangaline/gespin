a2004769f9d7f9c3da54e1ac7fa630f350b480cb
Evan Sangaline <esangaline@gmail.com>
2014-10-21 11:59:38 -0400

    Added radius and identity properties to Nucleons and tidied up the variable names to be more consistent.

--- inc/Nucleon.h
+++ inc/Nucleon.h
@@ -1,11 +1,23 @@
 #pragma once
 
+enum class NucleonIdentity { unspecified, proton, neutron, antineutron, antiproton };
 class Nucleon {
   private:
     double x, y, z;
+    double radius;
+    NucleonIdentity identity;
   public:
-    Nucleon(double x = 0, double y = 0, double z = 0);
+    Nucleon(double x = 0, double y = 0, double z = 0, double radius = 0.8768, NucleonIdentity identity = NucleonIdentity::unspecified);
 
+    //property getters
+    double Radius() { return radius; }
+    NucleonIdentity Identity() { return identity; }
+
+    //property setters
+    void SetRadius(double new_radius) { radius = new_radius; }
+    void SetIdentity(NucleonIdentity new_identity) { identity = new_identity; }
+
+    //position getters
     double X() { return x; }
     double Y() { return y; }
     double Z() { return z; }
@@ -13,11 +25,12 @@ class Nucleon {
     double Theta();
     double Phi();
 
-    void SetX(double newx) { x = newx; }
-    void SetY(double newy) { y = newy; }
-    void SetZ(double newz) { z = newz; }
+    //position setters
+    void SetX(double new_x) { x = new_x; }
+    void SetY(double new_y) { y = new_y; }
+    void SetZ(double new_z) { z = new_z; }
     void SetR(double new_r);
     void SetTheta(double new_theta);
     void SetPhi(double new_phi);
-    void SetPosition(double newx, double newy, double newz) { x = newx; y = newy; z = newz; }
+    void SetPosition(double new_x, double new_y, double new_z) { x = new_x; y = new_y; z = new_z; }
 };
