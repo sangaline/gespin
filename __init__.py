b80bf33ff5673cae90690fe2fc39f87dc9e7e2c4
Evan Sangaline <esangaline@gmail.com>
2014-10-21 14:28:14 -0400

    Added a plotting submodule which can currently draw nucleons as spheres in 3d. It's a bit slow to rotate around for a full nuclei but it is a pretty nice visualization.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
-#load the shared library (otherwise gespin.core won't work)
+#load the shared library (otherwise gespin.core won't work) ; and same for the others
 from gespin import core
+from gespin import plotting
 
-__all__ = ['core']
+__all__ = ['core', 'plotting']
